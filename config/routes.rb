eb42d5664048f74342a8aca3306ea2ac19ba8ecc
Michael Scherer <michael.c.scherer@gmail.com>
2016-10-18 21:43:10 -0600

    finished iteration 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists do
+    resources :songs, only: [:new, :create]
+  end
+  resources :songs, only: [:show]
+  resources :playlists, only: [:index]
 
 end
